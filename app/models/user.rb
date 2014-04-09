a7531d2d5b39a29ed2efce0de0feb9931d8930a0
gogosanka <jeffrey.jeanpierre@gmail.com>
2014-04-09 18:12:48 -0400

    Fixed protected attributes to allow attr_accessible by adding gem protected_attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :statuses
+
+  attr_accessible :email, :password, :password_confirmatino, :remember_me, :firstname, :lastname, :profilename
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
